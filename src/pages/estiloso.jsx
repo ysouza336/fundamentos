fef0c0f9c2090187219c551bcc56547c8fd1e4cd
ysouza336 <58566747+ysouza336@users.noreply.github.com>
2022-07-12 15:23:28 -0300

    nextjs

---
+++ src/pages/estiloso.jsx
@@ -0,0 +1,12 @@
+import Layout from '../components/Layout'
+import styles from '../styles/Estiloso.module.css'
+
+export default function Estiloso(){
+    return (
+        <Layout titulo="Exemplo de CSS Modularizado">
+            <div className={styles.roxo}>
+                <h1>Estilo usando CSS Módulos</h1>
+            </div>
+        </Layout>
+    )
+}
