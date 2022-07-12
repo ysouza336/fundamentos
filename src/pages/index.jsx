24975f2254223605925ca12f1a580bc47d7f2c94
ysouza336 <58566747+ysouza336@users.noreply.github.com>
2022-07-12 16:38:23 -0300

    navegacao-simples

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,18 @@
-import Link from 'next/link'
+import Navegador from '../components/Navegador'
 
 export default function Inicio(){
     return (
-        <div>
-          <Link href="/estiloso" >Estiloso</Link>
+        <div style={{
+          display:'flex',
+          justifyContent:'center',
+          alignItems: 'center',
+          flexWrap:'wrap',
+          height:'100vh'
+        }}>
+          <Navegador texto="Estiloso" destino="/estiloso" />
+          <Navegador texto="Exemplo" destino="/exemplo" cor="#9400d3" />
+          <Navegador texto="JSX" destino="/jsx" cor="crimson" />
+          <Navegador texto="Navegação #01" destino="/navegacao" cor="green" />
         </div>
     )
 }
